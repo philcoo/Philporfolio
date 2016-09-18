5beaae550822704561be712a3fc5a73f2530a267
Philco Michel <phil9231@gmail.com>
2016-09-18 13:25:07 -0400

    home page set and about pagein process

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '/home/aboutme'
+  
+  get 'home/aboutme'
+
+  get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
