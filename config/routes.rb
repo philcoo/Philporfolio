a6afa6d9bc5385d34f4fe89b142a6cd49d65d648
Philco Michel <phil9231@gmail.com>
2016-09-20 00:43:47 -0400

    background in about

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'home/aboutme'
 
   get 'home/index'
+
+  get 'home/manes' 
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
