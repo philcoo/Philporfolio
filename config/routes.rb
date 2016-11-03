35fc0f31e6fc6139b2143b079cc4a699a7dc769a
Philco Michel <phil9231@gmail.com>
2016-11-03 16:33:35 -0400

    updated porfoilo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+	Rails.application.routes.draw do
   resources :names
   root 'home#index'
   
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   get 'home/index'
 
-  get 'home/manes' 
+  get 'home/projects'
+
+  get 'home/contact' 
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
